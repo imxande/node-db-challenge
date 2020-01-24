a30d415e0fef2dd4764e8ce6ad8fa41fcbc6d314
imxande <alerodriguezlarosa@gmail.com>
2020-01-24 13:35:54 -0500

    adding resources method implemented, and tested on postman, API is creating a reasource now

--- project/project-model.js
+++ project/project-model.js
@@ -3,10 +3,16 @@ const db = require('../data/db.config.js');
 
 // export functions that i will us in the router
 module.exports = {
-    find
+    find,
+    addResc
 };
 
 // this function gets all the schemes
 function find(){
     return db('projects');
 }
+
+// function to add a resource
+function addResc(resource){
+    return db.insert(resource, '*').into('resources');
+};
--- project/project-router.js
+++ project/project-router.js
@@ -22,5 +22,19 @@ router.get('/', (req, res) => {
       res.status(500).json({ message: 'Failed to get projects' });
     });
   });
+
+  // adding resource
+  router.post('/resources', (req, res) =>{
+      const body = req.body
+    Projects.addResc(body)
+        .then(resource =>{
+            // return success status and send back the response
+            res.status(200).json(resource);
+        })
+        .catch(error =>{
+            console.log(error)
+            res.status(500).json({errorMessage: 'Could not add a resource, error on the server!'})
+        })
+  })
 // export router
 module.exports = router;
